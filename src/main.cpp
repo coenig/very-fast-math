b98fcd082a5b34bbcb35845c9c430dbb252ea2ea
Koenig Lukas (XC-AS/EAC1-ADA) <lukas.koenig@de.bosch.com>
2025-01-20 11:46:50 +0100

    minor

--- src/main.cpp
+++ src/main.cpp
@@ -8,26 +8,27 @@
 using namespace vfm;
 
 int main() {
+   const int num_lanes{ 2 };
    vfm::Vec2D A{ 100, 100 };
-   vfm::Vec2D B{ 200, 800 };
-   vfm::Vec2D C{ 450, 400 };
+   vfm::Vec2D C{ 800, 400 };
+   vfm::Vec2D B{ 200, 900 };
+
+   vfm::Image img{ 1000, 1000 };
+   img.fillImg(DARK_GREEN);
 
    Pol2D clothoid{};
    clothoid = clothoid.bezier(A, C, C, B, 0.001);
-   //clothoid.insertNodesOnExistingEdges(10, false);
    clothoid = clothoid.normalize();
-   vfm::Image img{ 1000, 1000 };
-   img.fillImg(DARK_GREEN);
    vfm::Pol2D arrow{};
    vfm::Pol2D arrow2{};
    arrow.createArrow(clothoid, 100);
-
-   auto segments = Pol2D::gestrichelterPfeil(clothoid, 3, 3);
-   std::cout << "segments.size(): " << segments.size() << std::endl;
-
    img.fillPolygon(arrow, vfm::GREY);
-   img.drawPolygon(arrow, vfm::WHITE);
-   img.drawPolygons(segments, { { nullptr, std::make_shared<Color>(WHITE)}, {nullptr, nullptr}});
+
+   for (int i = 0; i < num_lanes - 1; i++) {
+      auto segments = Pol2D::gestrichelterPfeil(clothoid, 3, 3);
+      img.drawPolygons(segments, { { nullptr, std::make_shared<Color>(WHITE)}, {nullptr, nullptr} });
+   }
+
    img.circle(A.x, A.y, 20, vfm::ORANGE);
    img.circle(C.x, C.y, 20, vfm::RED);
    img.circle(B.x, B.y, 20, vfm::ORANGE);
