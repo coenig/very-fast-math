f98a5bfea1af71035cf2c2281e8ed0b65d28bea7
Koenig Lukas (XC-AS/EAC1-ADA) <lukas.koenig@de.bosch.com>
2025-01-20 11:54:46 +0100

    Added curved-road example to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,33 @@ using namespace mc::trajectory_generator;
 
 int main(int argc, char* argv[])
 {
+   const int num_lanes{ 2 };
+   vfm::Vec2D A{ 100, 100 };
+   vfm::Vec2D C{ 750, 400 };
+   vfm::Vec2D B{ 200, 900 };
+
+   vfm::Image img{ 1000, 1000 };
+   img.fillImg(DARK_GREEN);
+
+   Pol2D clothoid{};
+   clothoid = clothoid.bezier(A, C, C, B, 0.001);
+   clothoid = clothoid.normalize();
+   vfm::Pol2D arrow{};
+   vfm::Pol2D arrow2{};
+   arrow.createArrow(clothoid, 100);
+   img.fillPolygon(arrow, vfm::GREY);
+
+   for (int i = 0; i < num_lanes - 1; i++) {
+      auto segments = Pol2D::gestrichelterPfeil(clothoid, 3, 3);
+      img.drawPolygons(segments, { { nullptr, std::make_shared<Color>(WHITE)}, {nullptr, nullptr} });
+   }
+
+   img.circle(A.x, A.y, 20, vfm::ORANGE);
+   img.circle(C.x, C.y, 20, vfm::RED);
+   img.circle(B.x, B.y, 20, vfm::ORANGE);
+   img.store("test");
+   termnate();
+
    //std::shared_ptr<HighwayTranslator> trans{ std::make_shared<Plain2DTranslator>() };
    //LaneSegment segment1{ 0, 0, 6 };
    //LaneSegment segment2{ 20, 2, 4 };
