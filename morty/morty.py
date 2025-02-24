d4539e1c9d1777b697c8f516ea25d221bf2d9fcd
Koenig Lukas <lukas.koenig@de.bosch.com>
2025-02-24 19:16:30 +0100

    int

--- morty/morty.py
+++ morty/morty.py
@@ -2,9 +2,7 @@ import gymnasium
 import highway_env
 from matplotlib import pyplot as plt
 #%matplotlib inline
-from subprocess import Popen, PIPE
-
-
+from ctypes import *
 
 env = gymnasium.make('highway-v0', render_mode='rgb_array', config={
     "action": {
@@ -19,6 +17,10 @@ env = gymnasium.make('highway-v0', render_mode='rgb_array', config={
 
 env.reset()
 
+morty_lib = CDLL('./lib/libvfm.so')
+morty_lib.morty.argtypes = [c_char_p]
+morty_lib.morty.restype = c_char_p
+
 action = env.unwrapped.action_type.actions_indexes["IDLE"]
 for i in range(15):
     obs, reward, done, truncated, info = env.step(action)
@@ -30,11 +32,10 @@ for i in range(15):
             input += str(val) + ","
         input += ";"
     
-    process = Popen(["bin/vfm", input], stdout=PIPE)
-    (output, err) = process.communicate()
-    exit_code = process.wait()
-    print(output)
-    action = env.unwrapped.action_type.actions_indexes[output.decode()]
+    res = morty_lib.morty(input.encode('utf-8'))
+    
+    print(res)
+    #action = env.unwrapped.action_type.actions_indexes[output.decode()]
 
 plt.imshow(env.render())
 plt.show()
