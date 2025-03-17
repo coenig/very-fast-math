eeea300a09e0ac53c8cab4005031283ac1ea2bf9
Koenig Lukas <lukas.koenig@de.bosch.com>
2025-03-17 14:08:29 +0100

    int

--- morty/morty.py
+++ morty/morty.py
@@ -3,6 +3,12 @@ import highway_env
 from matplotlib import pyplot as plt
 from ctypes import *
 
+def line_prepender(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
+        
 env = gymnasium.make('highway-v0', render_mode='rgb_array', config={
     "action": {
         "type": "MultiAgentAction",
@@ -18,7 +24,8 @@ env = gymnasium.make('highway-v0', render_mode='rgb_array', config={
     "absolute": True,
     "normalize": False,
     "clip": False,
-    "see_behind": True
+    "see_behind": True,
+    "order": "shuffled"
     },
     "simulation_frequency": 15,  # [Hz]
     "policy_frequency": 1,  # [Hz]
@@ -30,12 +37,15 @@ env = gymnasium.make('highway-v0', render_mode='rgb_array', config={
     "show_trajectories": True,
 })
 
-env.reset(seed=6) # 16, 21, 23
+env.reset(seed=0) # 3 6 7
 
 morty_lib = CDLL('./lib/libvfm.so')
 morty_lib.morty.argtypes = [c_char_p, c_char_p, c_size_t]
 morty_lib.morty.restype = c_char_p
 action = (2, 2, 2, 2, 2)
+open('lanes.txt', 'w').close()
+open('velocities.txt', 'w').close()
+
 for i in range(150):
     obs, reward, done, truncated, info = env.step(action)
     env.render()
@@ -56,6 +66,8 @@ for i in range(150):
     
     action_vec = []
     
+    lanes_store = ""
+    velocities_store = ""
     for res_str in res_strs:
         if res_str.replace('|', "").replace(',', "").replace(';', ""):
             both_str = res_str.split('|')
@@ -64,46 +76,46 @@ for i in range(150):
             
             delta_vels = delta_velocities_str.split(',')
             delta_lanes = delta_lanes_str.split(',')
+            
             delta_vel = float(delta_vels[0])
             delta_lane = float(delta_lanes[0])
-            print(delta_lanes)
-            if len(delta_lanes) > 2: # Last index is empty
-                delta_lane += float(delta_lanes[1])
+            delta_vel_acc = 0
+            delta_lane_acc = 0
+            
+            lanes_store += str(delta_lanes) + "\n"
+            velocities_store += str(delta_vels) + "\n"
+          
+            for i, el in enumerate(delta_lanes):
+                if i <= 4:
+                    if len(delta_lanes) > i + 1: # Last index is empty
+                        delta_lane_acc += float(delta_lanes[i])
 
-            if len(delta_lanes) > 3: # Last index is empty
-                delta_lane += float(delta_lanes[2])
-        
-            if len(delta_lanes) > 4: # Last index is empty
-                delta_lane += float(delta_lanes[3])
+            for i, el in enumerate(delta_vels):
+                if i <= 2:
+                    if len(delta_vels) > i + 1: # Last index is empty
+                        delta_vel_acc += float(delta_vels[i])
 
-            # if len(delta_lanes) > 5: # Last index is empty
-            #     delta_lane += float(delta_lanes[4])
-
-            # if len(delta_lanes) > 6: # Last index is empty
-            #     delta_lane += float(delta_lanes[5])
-        
-            if len(delta_vels) > 2: # Last index is empty
-               delta_vel += float(delta_vels[1])
-
-            if len(delta_vels) > 3: # Last index is empty
-               delta_vel += float(delta_vels[2])
-
-            if len(delta_vels) > 4: # Last index is empty
-               delta_vel += float(delta_vels[3])
-
-            if len(delta_vels) > 5: # Last index is empty
-               delta_vel += float(delta_vels[4])
-
-            if delta_lane < 0:
-               action_vec.append(0) # LANE_LEFT
-            elif delta_lane > 0:
-               action_vec.append(2) # LANE_RIGHT
+            if False:
+                pass
+            elif delta_vel_acc <= -6:
+               action_vec.append(4) # SLOWER
+            elif delta_vel_acc >= 6:
+               action_vec.append(3) # FASTER
+            elif delta_lane_acc < 0:
+               action_vec.append(2) # LANE_LEFT
+            elif delta_lane_acc > 0:
+               action_vec.append(0) # LANE_RIGHT
             elif delta_vel > 0:
                action_vec.append(3) # FASTER
             elif delta_vel < 0:
                action_vec.append(4) # SLOWER
             else:
                action_vec.append(1) # IDLE
+
+    line_prepender('lanes.txt', lanes_store)
+    line_prepender('lanes.txt', "")
+    line_prepender('velocities.txt', velocities_store)
+    line_prepender('velocities.txt', "")
     
     if not action_vec:
         print("WARNING: No CEX received.")
