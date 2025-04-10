50efa713abd76f143bffd6b4e6f0a46ebca54d50
Lukas Koenig <46921031+lukas-koenig-bosch@users.noreply.github.com>
2025-04-10 14:04:36 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -23,9 +23,6 @@ using namespace mc::trajectory_generator;
 
 int main(int argc, char* argv[])
 {
-   test::paintExampleRoadGraphCrossing(true);
-   termnate();
-
    //vfm::test::runTests();
    //termnate();
 
