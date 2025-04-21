8d9fa35a9b3a3648a1692860300905230d56e0fd
Koenig Lukas (XC-AS/EAC1-ADA) <lukas.koenig@de.bosch.com>
2025-04-21 22:50:05 +0200

    Basic trigonometry functions

--- include/terms_trigonometry.h
+++ include/terms_trigonometry.h
@@ -10,8 +10,7 @@
 
 namespace vfm {
 
-class TermSin :
-   public TermArith
+class TermSin : public TermArith
 {
 protected:
 #if defined(ASMJIT_ENABLED)
@@ -24,22 +23,105 @@ public:
    OperatorStructure getOpStruct() const override;
    std::shared_ptr<Term> getInvPattern(int argNum) override;
    float eval(const std::shared_ptr<DataPack>& varVals, const std::shared_ptr<FormulaParser> parser = nullptr, const bool suppress_sideeffects = false) override;
-   std::set<MetaRule> getSimplificationRules(const std::shared_ptr<FormulaParser> parser) const override;
    virtual std::shared_ptr<Term> copy(const std::shared_ptr<std::map<std::shared_ptr<Term>, std::shared_ptr<Term>>> already_copied = nullptr) override;
-
-   void serialize(
-      std::stringstream& os, 
-      const std::shared_ptr<MathStruct> highlight,
-      const SerializationStyle style,
-      const SerializationSpecial special,
-      const int indent,
-      const int indent_step,
-      const int line_len,
-      const std::shared_ptr<DataPack> data,
-      std::set<std::shared_ptr<const MathStruct>>& visited) const override;
 
    TermSin(const std::vector<std::shared_ptr<Term>>& opds);
    TermSin(const std::shared_ptr<Term> opd1);
+};
+
+class TermCos : public TermArith
+{
+protected:
+#if defined(ASMJIT_ENABLED)
+   std::shared_ptr<x86::Xmm> createSubAssembly(asmjit::x86::Compiler& cc, const std::shared_ptr<DataPack>& d, const std::shared_ptr<FormulaParser>& p = nullptr) override;
+#endif
+
+public:
+   const static OperatorStructure my_struct;
+
+   OperatorStructure getOpStruct() const override;
+   std::shared_ptr<Term> getInvPattern(int argNum) override;
+   float eval(const std::shared_ptr<DataPack>& varVals, const std::shared_ptr<FormulaParser> parser = nullptr, const bool suppress_sideeffects = false) override;
+   virtual std::shared_ptr<Term> copy(const std::shared_ptr<std::map<std::shared_ptr<Term>, std::shared_ptr<Term>>> already_copied = nullptr) override;
+
+   TermCos(const std::vector<std::shared_ptr<Term>>& opds);
+   TermCos(const std::shared_ptr<Term> opd1);
+};
+
+class TermTan : public TermArith
+{
+protected:
+#if defined(ASMJIT_ENABLED)
+   std::shared_ptr<x86::Xmm> createSubAssembly(asmjit::x86::Compiler& cc, const std::shared_ptr<DataPack>& d, const std::shared_ptr<FormulaParser>& p = nullptr) override;
+#endif
+
+public:
+   const static OperatorStructure my_struct;
+
+   OperatorStructure getOpStruct() const override;
+   std::shared_ptr<Term> getInvPattern(int argNum) override;
+   float eval(const std::shared_ptr<DataPack>& varVals, const std::shared_ptr<FormulaParser> parser = nullptr, const bool suppress_sideeffects = false) override;
+   virtual std::shared_ptr<Term> copy(const std::shared_ptr<std::map<std::shared_ptr<Term>, std::shared_ptr<Term>>> already_copied = nullptr) override;
+
+   TermTan(const std::vector<std::shared_ptr<Term>>& opds);
+   TermTan(const std::shared_ptr<Term> opd1);
+};
+
+class TermASin : public TermArith
+{
+protected:
+#if defined(ASMJIT_ENABLED)
+   std::shared_ptr<x86::Xmm> createSubAssembly(asmjit::x86::Compiler& cc, const std::shared_ptr<DataPack>& d, const std::shared_ptr<FormulaParser>& p = nullptr) override;
+#endif
+
+public:
+   const static OperatorStructure my_struct;
+
+   OperatorStructure getOpStruct() const override;
+   std::shared_ptr<Term> getInvPattern(int argNum) override;
+   float eval(const std::shared_ptr<DataPack>& varVals, const std::shared_ptr<FormulaParser> parser = nullptr, const bool suppress_sideeffects = false) override;
+   virtual std::shared_ptr<Term> copy(const std::shared_ptr<std::map<std::shared_ptr<Term>, std::shared_ptr<Term>>> already_copied = nullptr) override;
+
+   TermASin(const std::vector<std::shared_ptr<Term>>& opds);
+   TermASin(const std::shared_ptr<Term> opd1);
+};
+
+class TermACos : public TermArith
+{
+protected:
+#if defined(ASMJIT_ENABLED)
+   std::shared_ptr<x86::Xmm> createSubAssembly(asmjit::x86::Compiler& cc, const std::shared_ptr<DataPack>& d, const std::shared_ptr<FormulaParser>& p = nullptr) override;
+#endif
+
+public:
+   const static OperatorStructure my_struct;
+
+   OperatorStructure getOpStruct() const override;
+   std::shared_ptr<Term> getInvPattern(int argNum) override;
+   float eval(const std::shared_ptr<DataPack>& varVals, const std::shared_ptr<FormulaParser> parser = nullptr, const bool suppress_sideeffects = false) override;
+   virtual std::shared_ptr<Term> copy(const std::shared_ptr<std::map<std::shared_ptr<Term>, std::shared_ptr<Term>>> already_copied = nullptr) override;
+
+   TermACos(const std::vector<std::shared_ptr<Term>>& opds);
+   TermACos(const std::shared_ptr<Term> opd1);
+};
+
+class TermATan : public TermArith
+{
+protected:
+#if defined(ASMJIT_ENABLED)
+   std::shared_ptr<x86::Xmm> createSubAssembly(asmjit::x86::Compiler& cc, const std::shared_ptr<DataPack>& d, const std::shared_ptr<FormulaParser>& p = nullptr) override;
+#endif
+
+public:
+   const static OperatorStructure my_struct;
+
+   OperatorStructure getOpStruct() const override;
+   std::shared_ptr<Term> getInvPattern(int argNum) override;
+   float eval(const std::shared_ptr<DataPack>& varVals, const std::shared_ptr<FormulaParser> parser = nullptr, const bool suppress_sideeffects = false) override;
+   virtual std::shared_ptr<Term> copy(const std::shared_ptr<std::map<std::shared_ptr<Term>, std::shared_ptr<Term>>> already_copied = nullptr) override;
+
+   TermATan(const std::vector<std::shared_ptr<Term>>& opds);
+   TermATan(const std::shared_ptr<Term> opd1);
 };
 
 } // vfm
--- src/terms_trigonometry.cpp
+++ src/terms_trigonometry.cpp
@@ -5,22 +5,18 @@
 //============================================================================================================
 /// @file
 
-#include "term_ln.h"
-#include "meta_rule.h"
+#include "terms_trigonometry.h"
 
 using namespace vfm;
 
+// SIN
 const OperatorStructure TermSin::my_struct(OutputFormatEnum::prefix, AssociativityTypeEnum::left, OPNUM_SIN, PRIO_SIN, SYMB_SIN, false, false);
 
-OperatorStructure TermSin::getOpStruct() const
-{
-   return my_struct;
-}
-
-std::shared_ptr<Term> TermSin::getInvPattern(int argNum)
-{
-   return nullptr;
-}
+TermSin::TermSin(const std::vector<std::shared_ptr<Term>>& opds) : TermArith(opds) {}
+TermSin::TermSin(const std::shared_ptr<Term> opd1) : TermSin(std::vector<std::shared_ptr<Term>>{opd1}) {}
+OperatorStructure TermSin::getOpStruct() const { return my_struct; }
+std::shared_ptr<Term> TermSin::getInvPattern(int argNum) { return nullptr; }
+std::shared_ptr<Term> TermSin::copy(const std::shared_ptr<std::map<std::shared_ptr<Term>, std::shared_ptr<Term>>> already_copied) { return copySimple<TermSin>(already_copied); }
 
 float TermSin::eval(const std::shared_ptr<DataPack>& varVals, const std::shared_ptr<FormulaParser> parser, const bool suppress_sideeffects)
 {
@@ -28,13 +24,7 @@ float TermSin::eval(const std::shared_ptr<DataPack>& varVals, const std::shared_
    if (assembly_created_ == AssemblyState::yes) return (*fast_eval_func_)();
 #endif
 
-   return std::log(getOperands()[0]->eval(varVals, parser, suppress_sideeffects));
-}
-
-std::set<MetaRule> TermSin::getSimplificationRules(const std::shared_ptr<FormulaParser> parser) const
-{
-   std::set<MetaRule> vec = MathStruct::getSimplificationRules(parser);
-   return vec;
+   return std::sin(getOperands()[0]->eval(varVals, parser, suppress_sideeffects));
 }
 
 #if defined(ASMJIT_ENABLED)
@@ -44,37 +34,127 @@ std::shared_ptr<x86::Xmm> TermSin::createSubAssembly(asmjit::x86::Compiler& cc, 
 }
 #endif
 
-TermSin::TermLn(const std::vector<std::shared_ptr<Term>>& opds) : TermArith(opds)
+// COS
+const OperatorStructure TermCos::my_struct(OutputFormatEnum::prefix, AssociativityTypeEnum::left, OPNUM_COS, PRIO_COS, SYMB_COS, false, false);
+
+TermCos::TermCos(const std::vector<std::shared_ptr<Term>>& opds) : TermArith(opds) {}
+TermCos::TermCos(const std::shared_ptr<Term> opd1) : TermCos(std::vector<std::shared_ptr<Term>>{opd1}) {}
+OperatorStructure TermCos::getOpStruct() const { return my_struct; }
+std::shared_ptr<Term> TermCos::getInvPattern(int argNum) { return nullptr; }
+std::shared_ptr<Term> TermCos::copy(const std::shared_ptr<std::map<std::shared_ptr<Term>, std::shared_ptr<Term>>> already_copied) { return copySimple<TermCos>(already_copied); }
+
+float TermCos::eval(const std::shared_ptr<DataPack>& varVals, const std::shared_ptr<FormulaParser> parser, const bool suppress_sideeffects)
 {
+#if defined(ASMJIT_ENABLED)
+   if (assembly_created_ == AssemblyState::yes) return (*fast_eval_func_)();
+#endif
+
+   return std::cos(getOperands()[0]->eval(varVals, parser, suppress_sideeffects));
 }
 
-TermSin::TermLn(const std::shared_ptr<Term> opd1) : TermLn(std::vector<std::shared_ptr<Term>>{opd1})
+#if defined(ASMJIT_ENABLED)
+std::shared_ptr<x86::Xmm> TermCos::createSubAssembly(asmjit::x86::Compiler& cc, const std::shared_ptr<DataPack>& d, const std::shared_ptr<FormulaParser>& p)
 {
+   return nullptr;
+}
+#endif
+
+// TAN
+const OperatorStructure TermTan::my_struct(OutputFormatEnum::prefix, AssociativityTypeEnum::left, OPNUM_TAN, PRIO_TAN, SYMB_TAN, false, false);
+
+TermTan::TermTan(const std::vector<std::shared_ptr<Term>>& opds) : TermArith(opds) {}
+TermTan::TermTan(const std::shared_ptr<Term> opd1) : TermTan(std::vector<std::shared_ptr<Term>>{opd1}) {}
+OperatorStructure TermTan::getOpStruct() const { return my_struct; }
+std::shared_ptr<Term> TermTan::getInvPattern(int argNum) { return nullptr; }
+std::shared_ptr<Term> TermTan::copy(const std::shared_ptr<std::map<std::shared_ptr<Term>, std::shared_ptr<Term>>> already_copied) { return copySimple<TermTan>(already_copied); }
+
+float TermTan::eval(const std::shared_ptr<DataPack>& varVals, const std::shared_ptr<FormulaParser> parser, const bool suppress_sideeffects)
+{
+#if defined(ASMJIT_ENABLED)
+   if (assembly_created_ == AssemblyState::yes) return (*fast_eval_func_)();
+#endif
+
+   return std::tan(getOperands()[0]->eval(varVals, parser, suppress_sideeffects));
 }
 
-std::shared_ptr<Term> TermSin::copy(const std::shared_ptr<std::map<std::shared_ptr<Term>, std::shared_ptr<Term>>> already_copied)
+#if defined(ASMJIT_ENABLED)
+std::shared_ptr<x86::Xmm> TermTan::createSubAssembly(asmjit::x86::Compiler& cc, const std::shared_ptr<DataPack>& d, const std::shared_ptr<FormulaParser>& p)
 {
-   return copySimple<TermLn>(already_copied);
+   return nullptr;
+}
+#endif
+
+// ASIN
+const OperatorStructure TermASin::my_struct(OutputFormatEnum::prefix, AssociativityTypeEnum::left, OPNUM_ARCSIN, PRIO_ARCSIN, SYMB_ARCSIN, false, false);
+
+TermASin::TermASin(const std::vector<std::shared_ptr<Term>>& opds) : TermArith(opds) {}
+TermASin::TermASin(const std::shared_ptr<Term> opd1) : TermASin(std::vector<std::shared_ptr<Term>>{opd1}) {}
+OperatorStructure TermASin::getOpStruct() const { return my_struct; }
+std::shared_ptr<Term> TermASin::getInvPattern(int argNum) { return nullptr; }
+std::shared_ptr<Term> TermASin::copy(const std::shared_ptr<std::map<std::shared_ptr<Term>, std::shared_ptr<Term>>> already_copied) { return copySimple<TermASin>(already_copied); }
+
+float TermASin::eval(const std::shared_ptr<DataPack>& varVals, const std::shared_ptr<FormulaParser> parser, const bool suppress_sideeffects)
+{
+#if defined(ASMJIT_ENABLED)
+   if (assembly_created_ == AssemblyState::yes) return (*fast_eval_func_)();
+#endif
+
+   return std::asin(getOperands()[0]->eval(varVals, parser, suppress_sideeffects));
 }
 
-void vfm::TermSin::serialize(
-   std::stringstream& os, 
-   const std::shared_ptr<MathStruct> highlight, 
-   const SerializationStyle style,
-   const SerializationSpecial special,
-   const int indent,
-   const int indent_step, 
-   const int line_len,
-   const std::shared_ptr<DataPack> data,
-   std::set<std::shared_ptr<const MathStruct>>& visited) const
+#if defined(ASMJIT_ENABLED)
+std::shared_ptr<x86::Xmm> TermASin::createSubAssembly(asmjit::x86::Compiler& cc, const std::shared_ptr<DataPack>& d, const std::shared_ptr<FormulaParser>& p)
 {
-   if (style == SerializationStyle::nusmv) {
-      Failable::getSingleton()->addError("ln operator '" + getOptor() + "' cannot be translated to nusmv.");
-      os << "#Serialization-to-nusmv-not-possible-for-LN-operator";
-   } 
-   else {
-      MathStruct::serialize(os, highlight, style, special, indent, indent_step, line_len, data, visited);
-   }
-
-   visited.insert(shared_from_this());
+   return nullptr;
 }
+#endif
+
+// ACOS
+const OperatorStructure TermACos::my_struct(OutputFormatEnum::prefix, AssociativityTypeEnum::left, OPNUM_ARCCOS, PRIO_ARCCOS, SYMB_ARCCOS, false, false);
+
+TermACos::TermACos(const std::vector<std::shared_ptr<Term>>& opds) : TermArith(opds) {}
+TermACos::TermACos(const std::shared_ptr<Term> opd1) : TermACos(std::vector<std::shared_ptr<Term>>{opd1}) {}
+OperatorStructure TermACos::getOpStruct() const { return my_struct; }
+std::shared_ptr<Term> TermACos::getInvPattern(int argNum) { return nullptr; }
+std::shared_ptr<Term> TermACos::copy(const std::shared_ptr<std::map<std::shared_ptr<Term>, std::shared_ptr<Term>>> already_copied) { return copySimple<TermACos>(already_copied); }
+
+float TermACos::eval(const std::shared_ptr<DataPack>& varVals, const std::shared_ptr<FormulaParser> parser, const bool suppress_sideeffects)
+{
+#if defined(ASMJIT_ENABLED)
+   if (assembly_created_ == AssemblyState::yes) return (*fast_eval_func_)();
+#endif
+
+   return std::acos(getOperands()[0]->eval(varVals, parser, suppress_sideeffects));
+}
+
+#if defined(ASMJIT_ENABLED)
+std::shared_ptr<x86::Xmm> TermACos::createSubAssembly(asmjit::x86::Compiler& cc, const std::shared_ptr<DataPack>& d, const std::shared_ptr<FormulaParser>& p)
+{
+   return nullptr;
+}
+#endif
+
+// ATAN
+const OperatorStructure TermATan::my_struct(OutputFormatEnum::prefix, AssociativityTypeEnum::left, OPNUM_ARCTAN, PRIO_ARCTAN, SYMB_ARCTAN, false, false);
+
+TermATan::TermATan(const std::vector<std::shared_ptr<Term>>& opds) : TermArith(opds) {}
+TermATan::TermATan(const std::shared_ptr<Term> opd1) : TermATan(std::vector<std::shared_ptr<Term>>{opd1}) {}
+OperatorStructure TermATan::getOpStruct() const { return my_struct; }
+std::shared_ptr<Term> TermATan::getInvPattern(int argNum) { return nullptr; }
+std::shared_ptr<Term> TermATan::copy(const std::shared_ptr<std::map<std::shared_ptr<Term>, std::shared_ptr<Term>>> already_copied) { return copySimple<TermATan>(already_copied); }
+
+float TermATan::eval(const std::shared_ptr<DataPack>& varVals, const std::shared_ptr<FormulaParser> parser, const bool suppress_sideeffects)
+{
+#if defined(ASMJIT_ENABLED)
+   if (assembly_created_ == AssemblyState::yes) return (*fast_eval_func_)();
+#endif
+
+   return std::atan(getOperands()[0]->eval(varVals, parser, suppress_sideeffects));
+}
+
+#if defined(ASMJIT_ENABLED)
+std::shared_ptr<x86::Xmm> TermATan::createSubAssembly(asmjit::x86::Compiler& cc, const std::shared_ptr<DataPack>& d, const std::shared_ptr<FormulaParser>& p)
+{
+   return nullptr;
+}
+#endif
