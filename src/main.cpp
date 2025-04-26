16b4be2d775aaf51822dced160a20b791979358c
Koenig Lukas (XC-AS/EAC1-ADA) <lukas.koenig@de.bosch.com>
2025-04-26 06:06:10 +0200

    minor

--- src/main.cpp
+++ src/main.cpp
@@ -23,19 +23,20 @@ using namespace mc::trajectory_generator;
 
 int main(int argc, char* argv[])
 {
-   //mc::trajectory_generator::VisualizationLaunchers::quickGenerateGIFs(
-   //   { 
-   //      0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-   //      10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-   //      20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-   //      30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-   //      40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-   //   }, 
-   //   "../examples/gp_config_lanes=2_nonegos=1_segments=1", 
-   //   "debug_trace_array",
-   //   CexTypeEnum::smv,
-   //   false, false, false);
-   //termnate();
+   mc::trajectory_generator::VisualizationLaunchers::quickGenerateGIFs(
+      { 
+         0
+         //, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+         //10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+         //20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+         //30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+         //40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+      }, 
+      "../examples/gp_config_lanes=2_nonegos=1_segments=1", 
+      "debug_trace_array",
+      CexTypeEnum::smv,
+      false, false, false);
+   termnate();
 
    //vfm::test::runTests();
    //termnate();
