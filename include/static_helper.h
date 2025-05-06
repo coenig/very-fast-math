d2bc4d5a4176eb48d97793ad018b946efed8c82c
Koenig Lukas (XC-AS/EAC1-ADA) <lukas.koenig@de.bosch.com>
2025-05-06 16:03:50 +0200

    XML generation - int: state after triple PP

--- include/static_helper.h
+++ include/static_helper.h
@@ -760,6 +760,8 @@ public:
    static std::filesystem::file_time_type lastWritetimeOfFileSafe(const std::filesystem::path& filepath, const bool quiet = true);
 
    static void applyTimescaling(MCTrace& trace, const ScaleDescription& ts_description);
+   
+   static std::pair<double, double> cartesianToWGS84(const float x, const float y, const float z);
 
    static std::vector<std::string> getSafeCharactersAsciiLike();
    static std::map<std::string, int> getSafeCharactersAsciiLikeReverse();
