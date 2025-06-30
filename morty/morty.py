9b7ef357c02fe236f4841803b3e3d461366f95e4
Koenig Lukas <lukas.koenig@de.bosch.com>
2025-06-30 17:24:33 +0200

    64 succ

--- morty/morty.py
+++ morty/morty.py
@@ -130,7 +130,8 @@ for seedo in range(1, 101):
         # --- Above this line: seedo * 19
         # input += "$$$1$$$false$$$-0.5" (65 succesful)
         # input += "$$$1$$$false$$$-0.6" (~52% successful at 58)
-        input += "$$$1$$$false$$$-0.6"
+        # input += "$$$1$$$false$$$-0.4" (64 successful)
+        input += "$$$1$$$false$$$-0.4"
         
         if egos_x[4] < egos_x[3] and egos_x[3] < egos_x[2] and egos_x[2] < egos_x[1] and egos_x[1] < egos_x[0]:
             print("DONE") # Completion condition for position reversal SPEC.
