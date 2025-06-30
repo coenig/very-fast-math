5129a7e3c1981c57cbe852fae5bdb1ebbd6bd05b
Koenig Lukas <lukas.koenig@de.bosch.com>
2025-06-30 06:58:15 +0200

    65 succ

--- morty/morty.py
+++ morty/morty.py
@@ -53,7 +53,7 @@ for seedo in range(1, 101):
         "show_trajectories": True,
     })
 
-    env.reset(seed=seedo * 19) # Use some factor due to (unproven) suspicion that close-by seeds lead to similar starting positions.
+    env.reset(seed=seedo) # Use some factor due to (unproven) suspicion that close-by seeds lead to similar starting positions.
 
     action = ([0, 0], [0, 0], [0, 0], [0, 0], [0, 0])
     dpoints_y = [0, 0, 0, 0, 0, 0]     # The lateral position of the points the cars head towards.
@@ -126,8 +126,10 @@ for seedo in range(1, 101):
         # input += "$$$1.36125$$$false$$$0.50690625" (68 successful)
         # --- Above this line: old algorithm for heading correction in interactive_testing.cpp
         # input += "$$$1$$$false$$$-0.52" (65 successful)
-        # input += "$$$1$$$false$$$-0.50690625" (68 successful)
-        input += "$$$1$$$false$$$-0.50690625"
+        # input += "$$$1$$$false$$$-0.50690625" (68 successful; but without "blind" catching)
+        # input += "$$$1$$$false$$$-0.5" (65 succesful)
+        # --- Above this line: seedo * 19
+        input += "$$$1$$$false$$$-0.5"
         
         if egos_x[4] < egos_x[3] and egos_x[3] < egos_x[2] and egos_x[2] < egos_x[1] and egos_x[1] < egos_x[0]:
             print("DONE") # Completion condition for position reversal SPEC.
@@ -143,7 +145,7 @@ for seedo in range(1, 101):
         
         if res_str == "|;|;|;|;|;":
             print("No CEX found")
-            if nocex_count > 100: # Allow up to this many times being blind per run. (If in doubt: 10)
+            if nocex_count > 10: # Allow up to this many times being blind per run. (If in doubt: 10)
                 break
             nocex_count += 1
         
