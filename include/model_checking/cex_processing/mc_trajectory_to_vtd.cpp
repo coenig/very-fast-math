7d3690853501cb05a87d400b285cd817281c100e
delecodi <>
2025-07-11 13:28:57 +0200

    vtd export generate players and actions

--- include/model_checking/cex_processing/mc_trajectory_to_vtd.cpp
+++ include/model_checking/cex_processing/mc_trajectory_to_vtd.cpp
@@ -22,50 +22,21 @@ std::string VTDgenerator::generate()
 {
 	std::stringstream vtd_text{};
 
-	std::string players{
-		R"(<Player>
-			<Description Driver="DefaultDriver" Control="external" AdaptDriverToVehicleType="true" Type="AlfaRomeo_Brera_10_BiancoSpino" Name="Ego"/>
-			<Init>
-				<Speed Value="2.7777777777777779e+01"/>
-				<PosAbsolute X="1.1665091323852539e+02" Y="-1.9857120513916016e+00" Z="0.0000000000000000e+00" Direction="0.0000000000000000e+00" AlignToRoad="true"/>
-			</Init>
-		</Player>
-		<Player>
-			<Description Driver="DefaultDriver" Control="internal" AdaptDriverToVehicleType="true" Type="AlfaRomeo_Brera_10_BiancoSpino" Name="LeftFastOvertaker"/>
-			<Init>
-				<Speed Value="3.8888888888888886e+01"/>
-				<PosAbsolute X="2.0459701538085938e+01" Y="1.1126174926757812e+00" Z="0.0000000000000000e+00" Direction="6.2707495235650450e+00" AlignToRoad="true"/>
-			</Init>
-		</Player>
-		)"};
-
-	std::string player_actions{
-		R"(<PlayerActions Player="Ego">
-			<Action Name="keep_velo">
-				<PosAbsolute CounterID="" CounterComp="COMP_EQ" Radius="5.0000000000000000e+00" X="1.1665091323852539e+02" Y="-1.9857120513916016e+00" NetDist="false" CounterVal="0" Pivot="Ego"/>
-				<SpeedChange Rate="4.0000000000000000e+00" Target="2.7777777777777779e+01" Force="true" ExecutionTimes="1" ActiveOnEnter="true" DelayTime="0.0000000000000000e+00"/>
-			</Action>
-		</PlayerActions>
-		<PlayerActions Player="LeftFastOvertaker">
-			<Action Name="keep_velo">
-				<PosAbsolute CounterID="" CounterComp="COMP_EQ" Radius="5.0000000000000000e+00" X="2.0459701538085938e+01" Y="1.1126174926757812e+00" NetDist="false" CounterVal="0" Pivot="LeftFastOvertaker"/>
-				<SpeedChange Rate="4.0000000000000000e+00" Target="3.8888888888888886e+01" Force="true" ExecutionTimes="1" ActiveOnEnter="true" DelayTime="0.0000000000000000e+00"/>
-			</Action>
-		</PlayerActions>
+	std::string layout{R"(<Layout Database="../Databases/dummy.opt.osgb" File="../Databases/dummy.xodr"/>
 	)"};
-
-	std::string objects_control{
-		R"(
-	)"};
-
+	std::string players{generatePlayers()};
+	std::string player_actions{generatePlayerActions()};
+	std::string objects_control{generateMovingObjectsControl()};
 
 	// main scenario block
+	// TODO: what about road topology? dummy.xodr ok? set 1/2/3 lane straight road?
 	vtd_text << 
 R"(<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE Scenario>
 <Scenario RevMajor="2" RevMinor="2">
-	<Layout Database="../Databases/dummy.opt.osgb" File="../Databases/dummy.xodr"/>
-	<VehicleList ConfigFile="Distros/Current/Config/Players/Vehicles"/>
+	)"
+	<< layout <<
+ R"(<VehicleList ConfigFile="Distros/Current/Config/Players/Vehicles"/>
 	<DriverList ConfigFile="Distros/Current/Config/Players/driverCfg.xml"/>
 	<CharacterList ConfigFile="Distros/Current/Config/Players/Pedestrians"/>
 	<ObjectList ConfigFile="Distros/Current/Config/Players/Objects"/>
@@ -88,3 +59,98 @@ R"(<?xml version="1.0" encoding="UTF-8"?>
 
 	return vtd_text.str();
 }
+
+std::string VTDgenerator::generatePlayers()
+{
+	std::string players{};
+
+	for (auto& vehicle_name : m_interpreted_trace.getVehicleNames())
+	{
+		const trajectory_generator::FullTrajectory& trajectory = m_interpreted_trace.getVehicleTrajectory(vehicle_name);
+		// TODO: get abs pos not rel pos!
+		players += generatePlayer(vehicle_name, trajectory.front().second.at(PossibleParameter::vel_x), 
+			Vector3{trajectory.front().second.at(PossibleParameter::pos_x), trajectory.front().second.at(PossibleParameter::pos_y), 0.0});
+	}
+		
+		
+	return players;
+}
+
+std::string VTDgenerator::generatePlayer(std::string name, double speed, Vector3 pos)
+{
+	std::string control = (name == "ego") ? "external" : "internal";
+
+	std::string player{
+	 R"(<Player>
+			<Description Driver="DefaultDriver" Control=")" + control + R"(" AdaptDriverToVehicleType="true" Type="AlfaRomeo_Brera_10_BiancoSpino" Name=")" + name + R"("/>
+			<Init>
+				<Speed Value=")" + std::to_string(speed) + R"("/>
+				<PosAbsolute X=")" + std::to_string(pos.x) + R"(" Y=")" + std::to_string(pos.y) + R"(" Z="0.0" Direction="0.0" AlignToRoad="true"/>
+			</Init>
+		</Player>
+		)"};
+
+	return player;
+}
+
+std::string VTDgenerator::generatePlayerActions()
+{
+	std::string player_actions{};
+
+	for (auto& vehicle_name : m_interpreted_trace.getVehicleNames())
+	{
+		player_actions += generatePlayerAction(vehicle_name);
+	}
+
+	return player_actions;
+}
+
+std::string VTDgenerator::generatePlayerAction(std::string name)
+{
+	const trajectory_generator::FullTrajectory& trajectory = m_interpreted_trace.getVehicleTrajectory(name);
+
+	std::string action{};
+
+	for(const auto& trajectory_position : trajectory)
+	{
+		double time = trajectory_position.first;
+		double rate = 0.0; // TODO: get rate from trajectory
+		double target = trajectory_position.second.at(PossibleParameter::vel_x); // TODO: get target from trajectory
+
+		action += generateSpeedChangeAction("speed_change" + std::to_string((int)time), rate, target, time);
+	}
+
+	std::string player_action{
+	 R"(<PlayerActions Player=")" + name + R"(">
+		)" + action + R"(</PlayerActions>
+		)"};
+
+	return player_action;
+}
+
+std::string VTDgenerator::generateSpeedChangeAction(std::string name, double rate, double target, double time)
+{
+	std::string speed_change_action{
+	 R"(<SpeedChange Rate=")" + std::to_string(rate) + R"(" Target=")" + std::to_string(target) + R"(" ExecutionTimes="1" ActiveOnEnter="true" DelayTime=")" + std::to_string(time) + R"("/>)"
+	};
+
+	std::string action{
+	 R"(	<Action Name=")" + name + R"(">
+				<PosAbsolute CounterID="" CounterComp="COMP_EQ" Radius="5.0000000000000000e+00" X="1.1665091323852539e+02" Y="-1.9857120513916016e+00" NetDist="false" CounterVal="0" Pivot="Ego"/>
+				)" + speed_change_action + R"(
+			</Action>
+		)"};	
+
+	return action;
+}
+
+std::string VTDgenerator::generateMovingObjectsControl()
+{
+	return "\n\t";
+}
+
+std::string VTDgenerator::generatePolylinePathShape(std::string name, std::vector<Vector3> waypoints)
+{
+	return "";
+}
+
