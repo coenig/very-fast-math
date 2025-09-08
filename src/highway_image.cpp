7739c356b7ef6b4c529f3769dc460c8bbc900e28
Koenig Lukas (XC-AS/EAC1-ADA) <lukas.koenig@de.bosch.com>
2025-09-08 10:06:34 +0200

    Examples with ego and nonegos

--- src/highway_image.cpp
+++ src/highway_image.cpp
@@ -1248,7 +1248,7 @@ void vfm::HighwayImage::paintRoadGraph(
 
    label:
    setTranslator(old_trans);
-   //DRAW_STRAIGHT_ROAD_OR_CARS(RoadDrawingMode::ghosts_only); // For debugging.
+   DRAW_STRAIGHT_ROAD_OR_CARS(RoadDrawingMode::ghosts_only); // For debugging.
    DRAW_STRAIGHT_ROAD_OR_CARS(RoadDrawingMode::cars);
    setTranslator(old_trans);
 }
