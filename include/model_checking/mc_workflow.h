e2df4aaec492d82801d6bdd9b67a162d1e4c79b3
koeniglu@gmx.net <koeniglu@gmx.net>
2025-11-14 17:25:48 +0100

    int

--- include/model_checking/mc_workflow.h
+++ include/model_checking/mc_workflow.h
@@ -11,6 +11,8 @@
 
 namespace vfm
 {
+static const std::string PROSE_DESC_NAME{ "prose_scenario_description.txt" };
+
 namespace mc{
 
 class McWorkflow : public Failable
@@ -18,9 +20,21 @@ class McWorkflow : public Failable
 public:
    McWorkflow();
 
-   std::vector<std::string> McWorkflow::runMCJobs(const std::filesystem::path& working_dir);
+   std::vector<std::string> runMCJobs(
+      const std::filesystem::path& working_dir, 
+      const std::function<bool(const std::string& folder)> job_selector, 
+      const std::string& path_template);
+
+   void runMCJob(
+      const std::string& path_generated_raw, 
+      const std::string& config_name, 
+      const std::string& path_template);
+
+   void deleteMCOutputFromFolder(const std::string& path_generated, const bool actually_delete_gif); // ...or otherwise copy "waiting for" image.
 
 private:
+   std::mutex main_file_mutex_{};
+
 };
 
 } // mc
