6f9a15f37460386b2e22c9fd7ce4ea6165b6d8f9
Koenig Lukas (XC-AS/EAC1-ADA) <lukas.koenig@de.bosch.com>
2025-11-18 21:24:42 +0100

    Testcases based on individual names.

--- src/interactive_testing.cpp
+++ src/interactive_testing.cpp
@@ -827,8 +827,7 @@ int vfm::test::artifactRun(int argc, char* argv[])
          generated_dir,
          StaticHelper::removeLastFileExtension(inputs.getCmdOption(CMD_CEX_FILE)),
          mc::trajectory_generator::CexType(mc::trajectory_generator::CexTypeEnum::smv), // TODO: Make this parametrizable
-         true,
-         true);
+         mc::trajectory_generator::ALL_TESTCASE_MODES);
    }
 
    return success && !inputs.hasErrorOccurred()
