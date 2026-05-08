11ae93dbd08bd388dd48895f718ddacd1157a8e4
Lukas Koenig <lukas.koenig@de.bosch.com>
2026-05-08 08:26:23 +0200

    Commented in killing of scripts.

--- morty/morty.py
+++ morty/morty.py
@@ -377,7 +377,7 @@ for seedo in range(0, MAX_EXPs): # TODO: set ==> 0 again.
 
             @{{{mcinput}}}@.prepareInputForMortyUCD[{args.heading_adaptation}, {num_actual_lanes}, {num_technical_lanes}]
 
-            @{{nuXmv}}@.killAfter[15].Detach.setScriptVar[scriptID, force]
+            @{{nuXmv}}@.killAfter[300].Detach.setScriptVar[scriptID, force]
             @{{../../morty/envmodel_config.tpl.json}}@.runMCJobs[16]
             @{{scriptID}}@.scriptVar.StopScript
 
