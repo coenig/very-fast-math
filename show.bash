49c292537c64de05f161f0d1304f275f2b24ee93
Lukas Koenig <lukas.koenig@de.bosch.com>
2026-05-28 09:49:10 +0200

    bash script for eased analysis

---
+++ show.bash
@@ -0,0 +1,28 @@
+reset;
+clear;
+
+show () {
+   echo "$1                    ==> last $2 lines"
+   cat $1 | tail -n $2;
+   echo ''
+}
+
+num=${1:-10}
+
+names=`ls -d *`
+for eachfile in $names
+do
+   FILE1=$eachfile/examples
+   
+   if [ -d $FILE1 ]; then
+      subnames=`ls -d $FILE1/*`
+      for eachsubfile in $subnames
+      do
+         FILE=$eachsubfile/results.txt
+         if [ -f $FILE ]; then
+            (show "$FILE" $num)
+         fi
+      done
+   fi
+done
+
